d61279d8ddecbbeb4b64316c6ddd305d2c049866
Joel Chippindale <joel@joelchippindale.com>
2019-12-28 06:28:09 +0000

    Divide canvas into squares

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const canvas = document.getElementById('plot');
 const ctx = canvas.getContext('2d');
 
- ctx.fillStyle = 'rgb(200, 0, 0)';
- ctx.fillRect(10, 10, 50, 50);
+const plotSize = 30;
+const horizontalNumberOfPlots = Math.floor(1000 / plotSize);
+const verticalNumberOfPlots = Math.floor(500 / plotSize);
+
+let x, y;
+for (x = 0; x < horizontalNumberOfPlots; x++) {
+  for (y = 0; y < verticalNumberOfPlots; y++) {
+    ctx.fillStyle = 'rgb(0, ' + (255 - (x + y) * 4) + ', ' + ((x + y) * 4) + ')';
+    ctx.fillRect(x * plotSize, y * plotSize, (x + 1) * plotSize, (y + 1) * plotSize);
+  }
+}
