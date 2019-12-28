e332e3aaa0a1bbc80c2fae6ff2cf0e76b1a4720f
Joel Chippindale <joel@joelchippindale.com>
2019-12-28 07:25:46 +0000

    Reorganising a bit

--- src/index.js
+++ src/index.js
@@ -9,26 +9,39 @@ const verticalNumberOfPlots = Math.floor(500 / plotSize);
 const topLeft = Complex(-2, 0.8);
 const plotToComplexScale = 3 / horizontalNumberOfPlots;
 
-let x, y;
-for (x = 0; x < horizontalNumberOfPlots; x++) {
-  for (y = 0; y < verticalNumberOfPlots; y++) {
-    let it = iterationsForPlot(x, y);
-    ctx.fillStyle = 'rgb(0, ' + (255 - (it * 4)) + ', ' + (it * 4) + ')';
-    ctx.fillRect(x * plotSize, y * plotSize, (x + 1) * plotSize, (y + 1) * plotSize);
+let n, m;
+for (n = 0; n < horizontalNumberOfPlots; n++) {
+  for (m = 0; m < verticalNumberOfPlots; m++) {
+    plot(n, m);
   }
 }
 
-function iterationsForPlot(x, y) {
-  const c = topLeft.add(new Complex(x * plotToComplexScale, -(y * plotToComplexScale)));
-  return interationsFor(c);
+function plot(n, m) {
+  const it = iterations(plotToComplex(n, m));
+  ctx.fillStyle = 'rgb(0, ' + (255 - (it.length * 4)) + ', ' + (it.length * 4) + ')';
+  const topLeftCoords = plotToCoordinates(n, m);
+  const bottomRightCoords = plotToCoordinates(n + 1, m + 1);
+  ctx.fillRect(topLeftCoords.x, topLeftCoords.y, bottomRightCoords.x, bottomRightCoords.y);
 }
 
-function interationsFor(c) {
+function plotToCoordinates(n, m) {
+ return { x: n * plotSize, y: m * plotSize }
+}
+
+function plotToComplex(n, m) {
+  return topLeft.add(new Complex(n * plotToComplexScale, -(m * plotToComplexScale)));
+}
+
+function iterations(c) {
   let i = 0
   let z = Complex.ZERO;
+  let iterations = [];
+
   while (i < 40 && ((Math.pow(z.re, 2) + Math.pow(z.im, 2)) < 4)) {
     i++;
     z = z.pow(2).add(c);
+    iterations.push(z);
   }
-  return i;
+
+  return iterations;
 }
