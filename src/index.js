25b234f40ff33f618fb407ed64d996274f7e5522
Joel Chippindale <joel@joelchippindale.com>
2019-12-28 07:51:32 +0000

    Replace squares of colour with plots

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const horizontalNumberOfPlots = Math.floor(1000 / plotSize);
 const verticalNumberOfPlots = Math.floor(500 / plotSize);
 const topLeft = Complex(-2, 0.8);
 const plotToComplexScale = 3 / horizontalNumberOfPlots;
+const lengthOfComplexAxisInPlot = 2;
+const complexScaleinPlot = plotSize / lengthOfComplexAxisInPlot;
 
 let n, m;
 for (n = 0; n < horizontalNumberOfPlots; n++) {
@@ -17,11 +19,23 @@ for (n = 0; n < horizontalNumberOfPlots; n++) {
 }
 
 function plot(n, m) {
-  const it = iterations(plotToComplex(n, m));
-  ctx.fillStyle = 'rgb(0, ' + (255 - (it.length * 4)) + ', ' + (it.length * 4) + ')';
-  const topLeftCoords = plotToCoordinates(n, m);
-  const bottomRightCoords = plotToCoordinates(n + 1, m + 1);
-  ctx.fillRect(topLeftCoords.x, topLeftCoords.y, bottomRightCoords.x, bottomRightCoords.y);
+  let its = iterations(plotToComplex(n, m));
+  const startPos = complexPlotToCoordinates(n, m, its.shift);
+  ctx.beginPath();
+  ctx.strokeStyle = '#333';
+  ctx.moveTo(startPos.x, startPos.y);
+  its.forEach((i) => {
+    const pos = complexPlotToCoordinates(n, m, i);
+    ctx.lineTo(pos.x, pos.y);
+  });
+  ctx.stroke();
+}
+
+function complexPlotToCoordinates(n, m, c) {
+  const topLeftOfPlot = plotToCoordinates(n, m);
+  const middle = { x: topLeftOfPlot.x + (plotSize / 2), y: topLeftOfPlot.y + (plotSize / 2) }
+
+  return { x: middle.x + (c.re * complexScaleinPlot), y: middle.y - (c.im * complexScaleinPlot) }
 }
 
 function plotToCoordinates(n, m) {
@@ -39,8 +53,8 @@ function iterations(c) {
 
   while (i < 40 && ((Math.pow(z.re, 2) + Math.pow(z.im, 2)) < 4)) {
     i++;
-    z = z.pow(2).add(c);
     iterations.push(z);
+    z = z.pow(2).add(c);
   }
 
   return iterations;
