9ffce1afc690e312edb8ba3621eed2c768635dcd
Joel Chippindale <joel@monkeysthumb.co.uk>
2021-11-22 06:44:55 +0000

    Prettify JS and CSS

--- src/index.js
+++ src/index.js
@@ -9,24 +9,24 @@ import {
 
 const urlParams = new URLSearchParams(window.location.search);
 
-const plotSize = urlParams.get('plotSize') || 30;
+const plotSize = urlParams.get("plotSize") || 30;
 let middle;
-if (urlParams.get('middle')) {
-  console.log(urlParams.get('middle'));
-  middle = Complex(urlParams.get('middle'));
+if (urlParams.get("middle")) {
+  console.log(urlParams.get("middle"));
+  middle = Complex(urlParams.get("middle"));
 } else {
   middle = Complex(-0.5);
 }
-const complexWidth = urlParams.get('complexWidth') || 3;
-const maxNumberOfIterations = urlParams.get('maxNumberOfIterations') || 40;
+const complexWidth = urlParams.get("complexWidth") || 3;
+const maxNumberOfIterations = urlParams.get("maxNumberOfIterations") || 40;
 
 function setFormField(name, value) {
-  document.getElementById(name).setAttribute('value', value);
+  document.getElementById(name).setAttribute("value", value);
 }
-setFormField('middle', middle);
-setFormField('complexWidth', complexWidth);
-setFormField('plotSize', plotSize);
-setFormField('maxNumberOfIterations', maxNumberOfIterations);
+setFormField("middle", middle);
+setFormField("complexWidth", complexWidth);
+setFormField("plotSize", plotSize);
+setFormField("maxNumberOfIterations", maxNumberOfIterations);
 
 const ctx = new C2S(900, 750);
 
@@ -34,7 +34,7 @@ const dimensions = gridDimensions(ctx.width, ctx.height, plotSize);
 const pixelsToComplexScale = complexWidth / ctx.width;
 
 ctx.lineWidth = plotSize / 40;
-ctx.lineCap = 'round';
+ctx.lineCap = "round";
 
 let n, m;
 for (n = 0; n < dimensions.horizontal; n++) {
@@ -43,9 +43,11 @@ for (n = 0; n < dimensions.horizontal; n++) {
   }
 }
 
-document.getElementById('plot').appendChild(ctx.getSvg());
-document.getElementById('download-svg').onclick = function() { saveFile(ctx); return false; }
-
+document.getElementById("plot").appendChild(ctx.getSvg());
+document.getElementById("download-svg").onclick = function () {
+  saveFile(ctx);
+  return false;
+};
 
 function plot(n, m) {
   let its = iterations(plotToComplex(n, m), maxNumberOfIterations);
@@ -65,35 +67,44 @@ function plot(n, m) {
 }
 
 function scaleForIterations(iterations) {
-  const max = Math.max(...iterations.map( c => Math.max(Math.abs(c.re), Math.abs(c.im)) ));
+  const max = Math.max(
+    ...iterations.map((c) => Math.max(Math.abs(c.re), Math.abs(c.im)))
+  );
   return plotSize / (max * 2);
 }
 
 function colour(iterationNumber) {
   const frequency = 6.2 / maxNumberOfIterations;
-  const red   = Math.sin(frequency * iterationNumber + 2) * 127 + 128;
+  const red = Math.sin(frequency * iterationNumber + 2) * 127 + 128;
   const green = Math.sin(frequency * iterationNumber + 4) * 127 + 128;
-  const blue  = Math.sin(frequency * iterationNumber + 0) * 127 + 128;
+  const blue = Math.sin(frequency * iterationNumber + 0) * 127 + 128;
 
-  return 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+  return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
 
 function complexPlotToCoordinates(n, m, c, scale) {
   const topLeftOfPlot = coordinatesForGridLocation(n, m, plotSize);
-  const middle = { x: topLeftOfPlot.x + (plotSize / 2), y: topLeftOfPlot.y + (plotSize / 2) }
+  const middle = {
+    x: topLeftOfPlot.x + plotSize / 2,
+    y: topLeftOfPlot.y + plotSize / 2,
+  };
 
-  return { x: middle.x + (c.re * scale), y: middle.y - (c.im * scale) }
+  return { x: middle.x + c.re * scale, y: middle.y - c.im * scale };
 }
 
 function plotToComplex(n, m) {
   const coords = coordinatesForGridLocation(n, m, plotSize);
-  return middle.add(new Complex((coords.x - ctx.width / 2) * pixelsToComplexScale, -((coords.y - ctx.height / 2) * pixelsToComplexScale)));
+  return middle.add(
+    new Complex(
+      (coords.x - ctx.width / 2) * pixelsToComplexScale,
+      -((coords.y - ctx.height / 2) * pixelsToComplexScale)
+    )
+  );
 }
 
 function saveFile(ctx) {
-  const blob = new Blob(
-    [ctx.getSerializedSvg()],
-    { type: "text/plain;charset=utf-8" }
-  );
-  saveAs(blob, 'mandlebrot-plot.svg');
+  const blob = new Blob([ctx.getSerializedSvg()], {
+    type: "text/plain;charset=utf-8",
+  });
+  saveAs(blob, "mandlebrot-plot.svg");
 }
--- src/mandlebrot-set.js
+++ src/mandlebrot-set.js
@@ -1,11 +1,14 @@
-import Complex from 'complex.js';
+import Complex from "complex.js";
 
 export default function iterations(c, maxNumberOfIterations) {
-  let i = 0
+  let i = 0;
   let z = Complex.ZERO;
   let iterations = [];
 
-  while (i < maxNumberOfIterations && ((Math.pow(z.re, 2) + Math.pow(z.im, 2)) < 4)) {
+  while (
+    i < maxNumberOfIterations &&
+    Math.pow(z.re, 2) + Math.pow(z.im, 2) < 4
+  ) {
     i++;
     iterations.push(z);
     z = z.pow(2).add(c);
